436f5595957af435dcf5974e671d86aa3bfae43a
csc530 <{ID}+{username}@users.noreply.github.com>
2022-10-06 01:10:27 -0400

    Class to extend TimOnly functonality
    Extending System Time class so it can encapsulate handling events and a specific console feature aswell

---
+++ Timed Theme/Clock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timed_Theme
+{
+	internal class Clock
+	{
+		private TimeOnly _time = TimeOnly.MinValue;
+
+		public event EventHandler<TickEventArgs>? OnTickHandler;
+		protected virtual void OnTick() => OnTickHandler?.Invoke(this, generateTickEventArgs());
+		public int tickInterval { get; set; }//make setter
+		public class TickEventArgs : EventArgs
+		{
+			public int interval;
+		}
+
+
+		TimeOnly Time
+		{
+			get { return _time; }
+			set
+			{
+				_time = value;
+				OnTick?.Invoke(value, generateTickEventArgs());
+			}
+		}
+
+		public Clock() { };
+
+		public Clock(String time)
+		{
+			Time = TimeOnly.Parse(time);
+		}
+
+		public start() { }
+		public stop() { }
+
+		TickEventArgs generateTickEventArgs() { }
+	}
+}
